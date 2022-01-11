b06fbd9b41e49aaff665a71050da3568abf230e7
Chloe Ekstrum <chloe@Chloes-MacBook-Pro.local>
2022-01-10 20:45:51 -0600

    add user show and finish user auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,4 +18,9 @@ class UsersController < ApplicationController
           render json: { errors: user.errors.full_messages }, status: :bad_request
         end
     end
+
+    def show
+        user = current_user
+        render json: user
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post "/users" => "users#create"
   get "/users" => "users#index"
+  get "/users/:id" => "users#show"
 
   post "/sessions" => "sessions#create"
+
+  
 end
