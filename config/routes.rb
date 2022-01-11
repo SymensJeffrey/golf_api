96e88cecccd9e0580f29d5011627e686fdcea31e
Chloe Ekstrum <chloe@Chloes-MacBook-Pro.local>
2022-01-10 20:43:23 -0600

    add session create

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post "/users" => "users#create"
   get "/users" => "users#index"
+
+  post "/sessions" => "sessions#create"
 end
