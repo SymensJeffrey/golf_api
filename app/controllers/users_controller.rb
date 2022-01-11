d93d0dd098b3b0b8205ce25f79539278bfba9f9d
Chloe Ekstrum <chloe@Chloes-MacBook-Pro.local>
2022-01-10 20:39:18 -0600

    add user create and index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+    skip_before_action :verify_authenticity_token
+    def index 
+        users = User.all 
+        render json: users.as_json
+    end
+
+    def create
+        user = User.new(
+          name: params[:name],
+          email: params[:email],
+          password: params[:password],
+          password_confirmation: params[:password_confirmation]
+        )
+        if user.save
+          render json: { message: "User created successfully" }, status: :created
+        else
+          render json: { errors: user.errors.full_messages }, status: :bad_request
+        end
+    end
 end
