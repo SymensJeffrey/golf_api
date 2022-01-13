ccc44c7fb44ab3c6dd8ad6e59331033973b887eb
Chloe Ekstrum <chloe@Chloes-MacBook-Pro.local>
2022-01-12 18:23:12 -0600

    add scores index

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,2 +1,11 @@
 class ScoresController < ApplicationController
+    
+    def index
+        if current_user
+            scores = Score.where(status: "active")
+            render json: scores.as_json
+        else
+            render json: [], status: :unauthorized
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
 
   post "/sessions" => "sessions#create"
 
-  
+  get "/scores" => "scores#index"
 end
