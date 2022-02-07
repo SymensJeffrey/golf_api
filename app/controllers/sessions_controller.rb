a5d8e76ae4d7106ffe2c5cac022e34e34e7f3fce
symensjeffrey <jeffrey.symens23@gmail.com>
2022-02-07 17:35:10 -0600

    add code to file

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
               user_id: user.id, # the data to encode
               exp: 24.hours.from_now.to_i # the expiration time
             },
-            Rails.application.credentials.fetch(:secret_key_base), # the secret key
+            Rails.application.credentials.fetch(de2139e6ee20cd397f8306bcf0fc2d9bfdcb1aa0342b78cc917118ddb16dfac0542b99dbb074aaeb7059485c28d570bbe13f72e96642e4ad77f81fa3d0822b8f), # the secret key
             "HS256" # the encryption algorithm
           )
           render json: { jwt: jwt, email: user.email, user_id: user.id }, status: :created
