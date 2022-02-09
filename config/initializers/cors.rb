ba297762d41419426cf665d9f0415d489e18bd83
symensjeffrey <jeffrey.symens23@gmail.com>
2022-02-09 17:12:18 -0600

    remove try from cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,20 +5,15 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#     allow do
-#       origins 'http://localhost:8080', 'https://golfscoreboard.netlify.app'
-  
-#       resource '*',
-#         headers: :any,
-#         methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#     end
-#   end
-
-
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins 'https://golfscoreboard.netlify.app'
-    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    allow do
+      origins 'http://localhost:8080', 'https://golfscoreboard.netlify.app'
+  
+      resource '*',
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    end
   end
-end
+
+
+
