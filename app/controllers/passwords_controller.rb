7558cc8a04ccec38454830ca16798265b27944a0
symensjeffrey <jeffrey.symens23@gmail.com>
2022-04-20 19:17:22 -0500

    add password token to reset process

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -2,6 +2,8 @@ class PasswordsController < ApplicationController
     skip_before_action :verify_authenticity_token
     def create
         @user = User.find_by(email: params[:email])
+        @user.reset_password_token = SecureRandom.hex(3)
+        @user.save
         PasswordResetMailer.with(user: @user).password_reset_email.deliver_later
 
         render json: {message: "Password Reset email sent"}
