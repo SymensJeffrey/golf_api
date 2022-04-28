7ab5325128d8b5a83afb36888430dd2a3ce88362
symensjeffrey <jeffrey.symens23@gmail.com>
2022-04-28 18:20:44 -0500

    fix typo in production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -124,7 +124,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'localhost:3000',
+    domain:               'golfscoreboard.netlify.app',
     user_name:            ENV["GMAIL_USERNAME"],
     password:             ENV["GMAIL_PASSWORD"],
     authentication:       'plain',
