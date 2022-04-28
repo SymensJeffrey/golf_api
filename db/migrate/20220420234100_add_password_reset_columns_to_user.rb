d949def1cee4db8fdbcf015a6a67b14276336575
symensjeffrey <jeffrey.symens23@gmail.com>
2022-04-28 18:10:14 -0500

    remove columns from migrations that dont exist

--- db/migrate/20220420234100_add_password_reset_columns_to_user.rb
+++ db/migrate/20220420234100_add_password_reset_columns_to_user.rb
@@ -1,7 +1,5 @@
 class AddPasswordResetColumnsToUser < ActiveRecord::Migration[6.1]
   def change
-    remove_column :users, :reset_password_sent_at
-    remove_column :users, :reset_password_token
     add_column :users, :reset_password_token, :string
     add_column :users, :reset_password_sent_at, :datetime
   end
