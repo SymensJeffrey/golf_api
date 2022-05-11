9f062b1bc9cea6389168498424891ed9f1c0279f
symensjeffrey <jeffrey.symens23@gmail.com>
2022-05-10 21:03:31 -0500

    add throught to scores

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_10_001723) do
+ActiveRecord::Schema.define(version: 2022_05_11_020229) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 2022_05_10_001723) do
     t.string "name"
     t.integer "course_id"
     t.string "to_par"
+    t.integer "through"
   end
 
   create_table "tournaments", force: :cascade do |t|
